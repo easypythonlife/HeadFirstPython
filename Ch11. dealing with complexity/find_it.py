7496a9f429009b0c9ec65da634f4025cf742a0ba
Jeon, Seung Il <si.j@navercorp.com>
2014-12-15 10:39:43 +0900

    finale

--- Ch11. dealing with complexity/find_it.py
+++ Ch11. dealing with complexity/find_it.py
@@ -1,7 +1,6 @@
 import time
 
 def find_closest(look_for, target_data):
-
     def whats_the_difference(first, second):
         if first == second:
             return(0)
@@ -9,7 +8,6 @@ def find_closest(look_for, target_data):
             return(first - second)
         else:
             return(second - first)
-
     max_diff = 9999999
     for each_thing in target_data:
         diff = whats_the_difference(each_thing, look_for)
